8fb91bdb447e825fd26f3656d2eba96207ef7732
obengkofijames <obengkofijames@gmail.com>
2025-03-29 22:53:47 +0000

    chore : mongo connect

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -5,8 +5,13 @@ import "dotenv/config";
 export const connectDB = async () => {
     try {
         if (process.env.MONGO_URI) {
-            await mongoose.connect(process.env.MONGO_URI);
+            mongoose.connect(process.env.MONGO_URI)
+            .then(() => {
+                console.log("Connected to MongoDB successfully");
+              });
             console.log("Database connection established ...")
+        }else{
+            console.error("MONGO_URI not found in .env file")
         }
     } catch (error) {
         console.error(error);
