289f38019e233a3988b9d3da4f150a428759b665
kkopoku <kk.opoku@outlook.com>
2025-03-29 13:32:19 +0000

    chore: configured routes for fetching transactions

--- backend/src/routes/transaction.route.ts
+++ backend/src/routes/transaction.route.ts
@@ -1,8 +1,10 @@
 import express from "express"
-import { checkTransaction } from "../controllers/transaction.controller";
+import { checkTransaction, getTransactions, getSingleTransaction } from "../controllers/transaction.controller";
 
 const transactionRouter = express.Router()
 
 transactionRouter.post("/screen", checkTransaction)
+transactionRouter.get("/", getTransactions)
+transactionRouter.get("/:id", getSingleTransaction)
 
 export default transactionRouter
